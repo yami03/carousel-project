d8470746bc34544bdb0677e0cb3306c881dfb1dc
sla <uouo33@naver.com>
2019-05-31 19:54:10 +0900

    변수명, 함수명 변경

--- app/index.js
+++ app/index.js
@@ -10,33 +10,33 @@ import 'styles/index.css';
 // @param { string } -- dotsClassName
 // @param { string } -- duration
 function Carousel(sliderClassName, arrowsClassName, dotsClassName, duration = '.3s') {
+  this.slider = Array.from(document.querySelector('.' + sliderClassName).children);
   this.arrows = Array.from(document.querySelectorAll('.' + arrowsClassName));
   this.dots = Array.from(document.querySelector('.' + dotsClassName).children);
-  this.slider = Array.from(document.querySelector('.' + sliderClassName).children);
-  this.beforeNum = 0;
-  this.state = false;
-  this.buttonState = null;
+  this.beforeIdx = 0;
+  this.isSliding = false;
+  this.isArrowClick = false;
 
-  this.durationSet(duration);
+  this.setDuration(duration);
   this.arrowClick();
   this.dotsClick();
 }
 
-Carousel.prototype.durationSet = function (duration) {
+Carousel.prototype.setDuration = function (duration) {
   document.getElementsByTagName("html")[0].style.setProperty('--duration', duration);
 }
 
 Carousel.prototype.arrowClick = function() {
   this.arrows.forEach(arrow => arrow.addEventListener('click', () => {
-    if (this.state) return;
-    this.state = true;
-    this.buttonState = 'arrow';
+    if (this.isSliding) return;
+    this.isSliding = true;
+    this.isArrowClick = true;
     
-    let currentIdx = arrow.classList.contains('prev') ? this.beforeNum - 1 : this.beforeNum + 1;
+    let currentIdx = arrow.classList.contains('prev') ? this.beforeIdx - 1 : this.beforeIdx + 1;
     
     if (currentIdx < 0) {
       currentIdx = this.slider.length - 1;
-    } else if (currentIdx === this.slider.length ){
+    } else if (currentIdx === this.slider.length){
       currentIdx = 0
     }
 
@@ -47,9 +47,9 @@ Carousel.prototype.arrowClick = function() {
 
 Carousel.prototype.dotsClick = function() {
   this.dots.forEach((dot, idx) => dot.addEventListener('click', () => {
-    if (this.state) return;
-    if (this.beforeNum === idx) return;
-    this.state = true;
+    if (this.isSliding) return;
+    if (this.beforeIdx === idx) return;
+    this.isSliding = true;
 
     this.dotsChange(idx);
     this.slideShow(idx);
@@ -57,34 +57,33 @@ Carousel.prototype.dotsClick = function() {
 }
 
 Carousel.prototype.dotsChange = function (next) {
-  this.dots[this.beforeNum].classList.remove('is-selected');
+  this.dots[this.beforeIdx].classList.remove('is-selected');
   this.dots[next].classList.add('is-selected');
 }
 
 Carousel.prototype.slideShow = function(idx) {
-  const beforeSlide = this.slider[this.beforeNum];
+  const beforeSlide = this.slider[this.beforeIdx];
 
-  const lastToFirst = (this.beforeNum === this.slider.length - 1 && idx === 0);
-  const firstToLast = (this.beforeNum === 0 && idx === this.slider.length - 1);
+  const isLastToFirst = (this.beforeIdx === this.slider.length - 1 && !idx);
+  const isFirstToLast = (!this.beforeIdx && idx === this.slider.length - 1);
 
-  const arrowNextLoop = (this.buttonState === 'arrow' && lastToFirst);
-  const arrowPrevLoop = (this.buttonState === 'arrow' && firstToLast);
+  const isPrevLoop = (this.isArrowClick && isFirstToLast);
+  const isNextLoop = (this.isArrowClick && isLastToFirst);
 
   beforeSlide.classList.remove('right-show', 'left-show');
   
-  if ((this.beforeNum < idx && !arrowPrevLoop )|| arrowNextLoop) {
+  if ((this.beforeIdx < idx && !isPrevLoop ) || isNextLoop) {
     this.sliding(idx, 'left-hide', 'right-show');
-
   } else {
     this.sliding(idx, 'right-hide', 'left-show');
   }
 
-  this.beforeNum = idx;
-  this.buttonState = null;
+  this.beforeIdx = idx;
+  this.isArrowClick = false;
 }
 
 Carousel.prototype.sliding = function (idx, beforeClass, nextClass) {
-  const beforeSlide = this.slider[this.beforeNum];
+  const beforeSlide = this.slider[this.beforeIdx];
   const nextSlide = this.slider[idx];
 
   beforeSlide.classList.add(beforeClass);
@@ -92,8 +91,8 @@ Carousel.prototype.sliding = function (idx, beforeClass, nextClass) {
 
   nextSlide.addEventListener('animationend', () => {
     beforeSlide.classList.remove(beforeClass);
-    this.state = false;
+    this.isSliding = false;
   });
 }
 
-const carousel01 = new Carousel('slider-list', 'arrow', 'dots', '.4s');
+new Carousel('slider-list', 'arrow', 'dots', '.35s');
